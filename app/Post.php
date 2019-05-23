272b40a85e7aec58e2cf0d32ae90b929f20d17f7
nicomichelson <nicomichelson@gmail.com>
2019-05-23 02:16:51 +0000

    primer commit

--- app/Post.php
+++ app/Post.php
@@ -6,9 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-	protected fillable = [
-		'user_id', 'category_id', 'name', 'slug', 'excerpt', 'body', 'status', 'file'
-	];
+	protected $fillable = [	'user_id', 'category_id', 'name', 'slug', 'excerpt', 'body', 'status', 'file'];
 
     public function user(){
     	return $this->belongsTo(User::class);
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Auth::routes();
+
+/*Route::get('/home', 'HomeController@index')->name('home');*/
+
+Route::get('/', 'Web\PageController@blog')->name('blog');
